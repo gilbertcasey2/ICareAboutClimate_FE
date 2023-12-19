22cf228585285d25f602ac7b1bc8289dddd71627
Casey Gilbert <caseygilbert@Caseys-Laptop.local>
2023-12-18 20:02:17 -0500

    responsive updates

--- src/components/homePage/homeOne.jsx
+++ src/components/homePage/homeOne.jsx
@@ -6,7 +6,7 @@ import Water from "../../img/water.png";
 const HomeOne = (props) => {
 
   return <section className="homeOne">
-    <h3>I have opinions on</h3>
+    <h3>Your thoughts on</h3>
     <h1>climate change.</h1>
     <h4>A research project to understand how different communities across Portland, Maine feel about and engage with climate change.</h4>
     <Button isMain={true} isLink={true} linkPath="/contribute" buttonText="Make Your Voice Heard"></Button>
